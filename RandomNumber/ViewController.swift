23eacc0de481eeed85361ef09499033f51d815d6
Mariia <mariiavarsan@gmail.com>
2022-03-30 20:33:22 +0300

    Initial Commit

---
+++ RandomNumber/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomNumber
+//
+//  Created by Варсан Мария on 30.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
