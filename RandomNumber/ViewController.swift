f7c1c88be6ca5ed27b69daa34c9ab6e4ef3f9b7e
Mariia <mariiavarsan@gmail.com>
2022-03-30 21:46:49 +0300

    CAGradientLayer

--- RandomNumber/ViewController.swift
+++ RandomNumber/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var minimumValueLabel: UILabel!
+    @IBOutlet var maximumValueLabel: UILabel!
+    @IBOutlet var randomValueLabel: UILabel!
+    @IBOutlet var getRandomValueButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        getRandomValueButton.layer.cornerRadius = 10
+        
+        let color1 = UIColor(red: 153 / 255, green: 26 / 255, blue: 61 / 255, alpha: 1.0).cgColor
+        let color2 = UIColor(red: 243 / 255, green: 155 / 255, blue: 51 / 255 , alpha: 1.0).cgColor
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = self.view.bounds
+        gradientLayer.colors = [color1, color2]
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 0.0, y: 1.0)
+        self.view.layer.insertSublayer(gradientLayer, at: 0)
+        
     }
 
-
+    @IBAction func getRandomNumberButtonTapped() {
+        let minimumNumber = Int(minimumValueLabel.text ?? "") ?? 0
+        let maximumNumber = Int(maximumValueLabel.text ?? "") ?? 100
+        
+        randomValueLabel.text = String(Int.random(in: minimumNumber...maximumNumber))
+    }
+    
 }
 
