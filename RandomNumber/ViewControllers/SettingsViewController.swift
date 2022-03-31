5ab6caa5348f3d9e5e4d1f6eaad32c578c7637de
Mariia <mariiavarsan@gmail.com>
2022-03-31 22:44:41 +0300

    Settings view controller

---
+++ RandomNumber/ViewControllers/SettingsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SettingsViewController.swift
+//  RandomNumber
+//
+//  Created by Варсан Мария on 31.03.2022.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet var minimumValueTF: UITextField!
+    @IBOutlet var maximumValueTF: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+    
+}
--- RandomNumber/ViewControllers/ViewController.swift
+++ RandomNumber/ViewControllers/ViewController.swift
@@ -18,16 +18,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         getRandomValueButton.layer.cornerRadius = 10
         
-        let color1 = UIColor(red: 153 / 255, green: 26 / 255, blue: 61 / 255, alpha: 1.0).cgColor
-        let color2 = UIColor(red: 243 / 255, green: 155 / 255, blue: 51 / 255 , alpha: 1.0).cgColor
+        let color3 = UIColor(red: 209 / 255, green: 107 / 255, blue: 165 / 255 , alpha: 1.0).cgColor
+        let color2 = UIColor(red: 134 / 255, green: 168 / 255, blue: 231 / 255, alpha: 1.0).cgColor
+        let color1 = UIColor(red: 95 / 255, green: 251 / 255, blue: 241 / 255, alpha: 1.0).cgColor
         
         let gradientLayer = CAGradientLayer()
         gradientLayer.frame = self.view.bounds
-        gradientLayer.colors = [color1, color2]
+        gradientLayer.colors = [color1, color2, color3]
         gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
-        gradientLayer.endPoint = CGPoint(x: 0.0, y: 1.0)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
         self.view.layer.insertSublayer(gradientLayer, at: 0)
-        
     }
 
     @IBAction func getRandomNumberButtonTapped() {
